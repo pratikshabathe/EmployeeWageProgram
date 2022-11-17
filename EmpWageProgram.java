a7c0313a43a122e85293cb734a75c366e019ea8b
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-17 15:31:36 +0530

    create new branch

--- EmpWageProgram.java
+++ EmpWageProgram.java
@@ -4,47 +4,62 @@ import java.util.Random;
 
 public class EmpWageProgram{
 
-		public static void main(String[] args) {
-			
-		
-	 System.out.println("--Welcome to Employee Wage Computation Program--\n");
+	public static void main(String[] args) {
+	    System.out.println("--Welcome to Employee Wage Computation Program--\n");
 
-	    String attendanceString = EmpWageProgram.checkAttendance();
-	    System.out.println(attendanceString);
+	    System.out.println("Employee's wage for this month: " + getMonthlyWage() + "$");
 
-	    System.out.println("Employee's today's wage: " + getDailyWage(attendanceString));
-
-}
+	  }
 
 	  private static String checkAttendance() {
 	    Random random = new Random();
 	    int randomNum = random.nextInt();
 
 	    switch (randomNum) {
-	      case 1:
-	        return "employee is present full time";
+	    case 1:
+	      return "employee is present full time";
 
-	      case 2:
-	        return "employee is present part time";
+	    case 2:
+	      return "employee is present part time";
 
-	      default:
-	        return "employee is absent";
+	    default:
+	      return "employee is absent";
 	    }
 
 	  }
 
-	  private static int getDailyWage(String str) {
+	  private static int getDailyWage() {
 
 	   Byte empRatePerHr = 20;
 	   Byte hrsWorked = 0;
 	   Byte fullTimeHrs = 8;   //assumed
 	   Byte partTimeHrs = 4;   //assumed
 
+	   String str = checkAttendance();
+
 	   if (str.contains("full time")) hrsWorked = fullTimeHrs;
 	   else if (str.contains("part time")) hrsWorked = partTimeHrs;
 
 	   int dailyWage = empRatePerHr * hrsWorked;
 	   return dailyWage;
+	 }
 
-	  }
+
+	 private static int getMonthlyWage() {
+
+	  int monthlyWage = 0;
+	  short daysWorked;
+
+	  for (daysWorked = 0; daysWorked < 20;) {
+
+	    int dailyWage = getDailyWage(); //getting employee's daily wage for each day
+
+	    if (dailyWage > 0) {            //checking if the employee is present i.e. has dailyWage>0
+	      monthlyWage += dailyWage;
+	      daysWorked++;                 //only increasing number of days worked if the employee HAS worked, either full time or part time
+	      }
+	    }
+
+	    return monthlyWage;
+	 }
 }
